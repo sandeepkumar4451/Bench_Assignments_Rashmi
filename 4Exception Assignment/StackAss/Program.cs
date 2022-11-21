f7a51a33804dc102e685f74f3cd10c17cabdf79e
sandeepkumar4451 <95675408+sandeepkumar4451@users.noreply.github.com>
2022-11-21 19:29:56 +0530

    Add files via upload

---
+++ 4Exception Assignment/StackAss/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAss
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            StackException myStack = new StackException();
+
+            myStack.Push(10);
+            myStack.Push(20);
+            myStack.Push(30);
+            myStack.Push(40);
+            myStack.PrintStack();
+            // Console.WriteLine("Item popped from Stack : {1}", myStack.POP());
+            myStack.POP();
+            myStack.POP();
+            myStack.POP();
+            myStack.POP();
+            myStack.PrintStack();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 4Exception Assignment/StackAss/StackException.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAss
+{
+    public class StackException
+    {
+        static int MAX = 100;
+        int top = -1;
+        int[] Stack = new int[MAX];
+        int value;
+        public StackException()
+        {
+            top = -1;
+        }
+        public void Push(int data)
+        {
+            if (top == MAX - 1)
+            {
+                throw new Exception("Stack Overflow =");
+            }
+            else
+            {
+                Stack[++top] = data;
+            }
+        }
+        public int POP()
+        {
+            if (top == -1)
+            {
+                throw new Exception("Stack UnderFlow =");
+            }
+            else
+            {
+                value = Stack[top--];
+                return value;
+            }
+        }
+        public void PrintStack()
+        {
+            if (top == -1)
+            {
+                Console.WriteLine("Stack Is Empty");
+                return;
+            }
+            else
+            {
+                Console.WriteLine("Items in the Stack are :");
+                for (int i = top; i >= 0; i--)
+                {
+                    Console.WriteLine(Stack[i]);
+                }
+            }
+        }
+    }
+}
